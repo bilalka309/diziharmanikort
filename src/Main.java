fe5ed8901e1dcde54c4f62955c413693fdb0a59c
bilal karahan <bilalkarahan309@gmail.com>
2022-10-05 15:09:53 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {1, 2, 3, 4, 5};
+        double ho=0.0;
+        double hs=0.0;
+        for (int i = 0; i < numbers.length; i++) {
+
+            hs+=(1.0/numbers[i]);
+
+        }
+        ho=numbers.length/hs;
+        System.out.println(ho);
+
     }
 }
